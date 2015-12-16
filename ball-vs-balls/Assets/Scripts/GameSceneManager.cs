efd25042440732d8e8889a9925896358587fea26
sieg-lu <siegdt.lu@gmail.com>
2015-12-15 23:53:48 -0800

    Start game scene, generate boxes matrix

---
+++ ball-vs-balls/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class GameSceneManager : MonoBehaviour
+{
+    // ----------- private variables
+    private const int kMaxWidth = 100;
+    private const int kMaxHeight = 100;
+    private const float kIntervalBetweenBlocks = 0.05f;
+
+    private GameObject mBricksRoot = null;
+    private Vector3 mNormalBrickSize;
+
+    // ----------- public variables
+    public GameObject normalBrick;
+    public GameObject wallBrick;
+
+    public int width = 50;
+    public int height = 50;
+
+    // ----------- helper functions
+    private void SetupScene()
+    {
+        Assert.IsTrue(width > 0 && width < kMaxWidth);
+        Assert.IsTrue(height > 0 && height < kMaxHeight);
+
+        float start_x = (mNormalBrickSize.x + kIntervalBetweenBlocks) * (-width / 2.0f);
+        float start_z = (mNormalBrickSize.z + kIntervalBetweenBlocks) * (-height / 2.0f);
+        float end_x = (mNormalBrickSize.x + kIntervalBetweenBlocks) * (width / 2.0f);
+        float end_z = (mNormalBrickSize.z + kIntervalBetweenBlocks) * (height / 2.0f);
+
+        Assert.IsTrue(mBricksRoot == null);
+        mBricksRoot = new GameObject();
+        mBricksRoot.transform.position = this.transform.position;
+
+        int i = 0;
+        int j = 0;
+        float current_x = start_x;
+        float current_z = start_z;
+
+        for (i = 0, current_x = start_x;
+             i < width;
+             i++, current_x += (mNormalBrickSize.x + kIntervalBetweenBlocks))
+        {
+            for (j = 0, current_z = start_z;
+                 j < height;
+                 j++, current_z += (mNormalBrickSize.z + kIntervalBetweenBlocks))
+            {
+                Vector3 position = new Vector3(current_x, mNormalBrickSize.y / 2.0f + 0.05f, current_z);
+                GameObject current_brick = (GameObject)Instantiate(normalBrick, position, Quaternion.identity);
+
+                current_brick.name = "brick" + (i * height + j + 1).ToString();
+                current_brick.transform.parent = mBricksRoot.transform;
+            }
+        }
+    }
+    
+    // ----------- callback functions
+    void Start()
+    {
+        mNormalBrickSize = normalBrick.transform.localScale;
+        Debug.Log("normal brick's size: " + mNormalBrickSize.ToString());
+
+        SetupScene();
+    }
+	
+	void Update()
+    {
+	
+	}
+}
--- ball-vs-balls/Assets/Scripts/TestSceneManager.cs
+++ ball-vs-balls/Assets/Scripts/TestSceneManager.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TestSceneManager : MonoBehaviour
 {
     // private variables
     private Queue m_detonate_queue;
+    private float m_game_time;
+    private List<float> m_trigger_times;
+    private const float m_start_triggering_time = 2.0f;
+    private const float m_end_triggering_time = 5.0f;
+    private int m_current_index;
 
-    private void test_scene_detonate_boxes()
+    private void detonate_box()
     {
         if (m_detonate_queue.Count != 0)
         {
@@ -40,11 +46,32 @@ public class TestSceneManager : MonoBehaviour
         m_detonate_queue.Enqueue(box_3);
         m_detonate_queue.Enqueue(box_4);
 
-        InvokeRepeating("test_scene_detonate_boxes", 2.0f, 0.5f);
+        const int length = 10;
+        m_trigger_times = new List<float>();
+        for (int i = 0; i < length; i++)
+        {
+            float current = 0.5f / Mathf.Exp((float) (i + 10) / (float) (length + 10));
+
+            if (i != 0)
+            {
+                current += m_trigger_times[i - 1];
+            }
+
+            m_trigger_times.Add(current);
+            Debug.Log(m_trigger_times[i]);
+        }
+
+        m_current_index = 0;
+//        InvokeRepeating("test_scene_detonate_boxes", 2.0f, 0.3f);
     }
 	
 	void Update()
     {
-	    
-	}
+        m_game_time += Time.deltaTime;
+        if (m_current_index < m_trigger_times.Count && m_game_time > m_trigger_times[m_current_index])
+        {
+            detonate_box();
+            m_current_index++;
+        }
+    }
 }
